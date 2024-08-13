0b77d1763b611fd68a05d3a35539b935a7cf3f59
Vanessa Dirner <vdirner@gmail.com>
2024-08-13 14:18:14 -0600

    update sql table setup values

--- db.sql
+++ db.sql
@@ -9,25 +9,22 @@ CREATE TABLE employee (
     email varchar(30) NOT NULL UNIQUE,
     startdate date,
     enddate date,
-    active bit, --set default to active
-    createdby char(25) NOT NULL,
+    active bit DEFAULT 1, --set default to active
+    createdby varchar(25) NOT NULL,
     createdate DATE NOT NULL,
-    modifiedby varchar(25) NOT NULL,
-    modifieddate date NOT NULL
+    modifiedby varchar(25) ,
+    modifieddate date 
     PRIMARY KEY (personID)
 );
 
-insert into employee (firstname, lastname, phonenumber, email, startdate, enddate, active)
-values ('Joe', 'Smith', 4440942039, 'jsmith@project.ca', 7/22/2024, 8/1/2024, 1)
-
 CREATE TABLE department (
     deptID UNIQUEIDENTIFIER NOT NULL,
-    deptname varchar(25) NOT NULL,
-    abbreviation varchar(5) NOT NULL UNIQUE,
-    createdby char(25) NOT NULL,
+    deptname varchar(25) NOT NULL UNIQUE,
+    abbreviation varchar(10) NOT NULL UNIQUE,
+    createdby varchar(25) NOT NULL,
     createdate DATE NOT NULL,
-    modifiedby varchar(25) NOT NULL,
-    modifieddate date NOT NULL
+    modifiedby varchar(25),
+    modifieddate date 
     PRIMARY KEY (deptID)
 );
 
@@ -35,13 +32,10 @@ CREATE TABLE assignment (
     assignmentID UNIQUEIDENTIFIER NOT NULL,
     personID UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES employee(personID),
     deptID UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES department(deptID),
-    createdby char(25) NOT NULL,
+    createdby varchar(25) NOT NULL,
     createdate DATE NOT NULL,
-    modifiedby varchar(25) NOT NULL,
-    modifieddate date NOT NULL,
+    modifiedby varchar(25),
+    modifieddate date,
     PRIMARY KEY (assignmentID)
 );
-
--- drop table employee;
--- drop table department;
 
