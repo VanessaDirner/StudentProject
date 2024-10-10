ad79285265f1272af3f3924fa8bea3bf142dc21b
Vanessa Dirner <vdirner@gmail.com>
2024-10-10 16:18:31 -0600

    start winforms

---
+++ excercise/app/StudentWindowsFormsApp/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // add a xyz like person
+
+            // call api here
+
+        }
+
+        private void button1_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
+    }
+}
