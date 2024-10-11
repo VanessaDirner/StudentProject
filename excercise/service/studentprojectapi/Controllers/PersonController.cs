b16346443ff19e8eb595df2ab49dc55ae50f468f
Vanessa Dirner <vdirner@gmail.com>
2024-10-10 20:35:37 -0600

    adding more http error codes to person api

--- excercise/service/studentprojectapi/Controllers/PersonController.cs
+++ excercise/service/studentprojectapi/Controllers/PersonController.cs
@@ -6,6 +6,7 @@ using studentprojectapi.Services;
 
 namespace studentprojectapi.Controllers
 {
+    #region setup
     [ApiController]
     [Route("api/[controller]")]
     public class PersonController : ControllerBase
@@ -20,9 +21,26 @@ namespace studentprojectapi.Controllers
             _personService = service;
          }
 
+        #endregion setup
 
+        #region api
+        /// <summary>
+        /// This is a summary comment thingy
+        /// </summary>
+        /// <param>
+        /// THis is a parameter comment thingy
+        /// </param>
+        /// <example>
+        /// This is an example comment?!?!?!
+        /// </example>
+        /// <returns>
+        /// Returns result of deletion or an error
+        /// </returns>
         // this is an api endpoint
         [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
         public async Task<ActionResult<PersonDTO>> GetEmployees()
         {
             // list of employee objects
@@ -36,35 +54,80 @@ namespace studentprojectapi.Controllers
             // then add httpget 400, 200 etc specified for right above api endpoint 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="personDTO"></param>
+        /// <returns></returns>
         [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
         public async Task<ActionResult<PersonDTO>> AddEmployee([FromBody] PersonDTO personDTO ) 
        {
             // get add details from body of get request
             // personDTO is the format in which the body of the request arrives
             // it's specified on the swagger page 
             // we'll send this to the service that will translate this to the model of the database
+            
+            try
+            {
+                employee returnemployee = await _personService.AddEmployeeAsync(personDTO);
 
-            employee returnemployee = await _personService.AddEmployeeAsync(personDTO);
-                        
-            return Ok(returnemployee);
+                return Ok(returnemployee);
+            }
+            catch
+            {
+                // either there's a person in the list or not, no way to mess up the api get request here so no need for 4XX error
+                // 5XX will be returned anyways
+                return BadRequest();
+            }
+
+
+
+
         }
 
+
+        /// <summary>
+        /// /
+        /// </summary>
+        /// <param name="updatepersonDTO"></param>
+        /// <returns></returns>
         [HttpPut]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
         // I don't want to accept a new GUID
         // I don't want createddate and createdby to be updated either
         // I need to match details with a GUID in table
         public async Task<ActionResult<PersonDTO>> ModifyEmployee([FromBody] PersonDTO updatepersonDTO)
         {
-            employee returnemployee = await _personService.ModifyEmployeeService(updatepersonDTO);
+            try
+            {
+                employee returnemployee = await _personService.ModifyEmployeeService(updatepersonDTO);
 
-            return Ok(returnemployee);
+                return Ok(returnemployee);
+            }
+            catch (Exception ex)
+            {
+               return StatusCode(404, "this is the not found error");
+                
+            }
+
         }
 
-
+        /// <summary>
+        /// /
+        /// </summary>
+        /// <param name="deletepersonDTO"></param>
+        /// <returns></returns>
         [HttpDelete]
         // this is documenting what kind of return types are possible (shows in swagger)
         [ProducesResponseType(200)]
-       public async Task<ActionResult> DeleteEmployee([FromBody] deletepersonDTO deletepersonDTO)
+        [ProducesResponseType(404)]
+        [ProducesResponseType(418)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult> DeleteEmployee([FromBody] deletepersonDTO deletepersonDTO)
         {
             try
             {
@@ -77,6 +140,10 @@ namespace studentprojectapi.Controllers
                 {
                     return StatusCode(404, "this is the not found error");
                 }
+                else if(e.Message == "Employee is assigned to a department. Please remove the assignment before deleting employee.")
+                {
+                    return StatusCode(418, "not sure what the actual error code is supposed to be but would you like some tea?");
+                }
                 else
                 {
                     return StatusCode(500, "uknown server error");
@@ -88,6 +155,6 @@ namespace studentprojectapi.Controllers
             // later, do I want to return something?
         }
 
-
+        #endregion api
     }
 }
