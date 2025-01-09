a6673d9790848f2affbb2be68327e039cd906564
Vanessa Dirner <vanessadirner.work@gmail.com>
2025-01-09 10:29:07 -0700

    added check for employee before deleting department

--- excercise/app/StudentWindowsFormsApp/modifydepartment forms/DeleteDepartment.cs
+++ excercise/app/StudentWindowsFormsApp/modifydepartment forms/DeleteDepartment.cs
@@ -28,21 +28,29 @@ namespace StudentWindowsFormsApp
             bool isvalid = true;
 
            string depttodelete = txt_deptname_todelete.Text;
-            department department = _studentprojectEntities.departments.SingleOrDefault(x => x.deptname == depttodelete);
+            department departmentobject = _studentprojectEntities.departments.SingleOrDefault(x => x.deptname == depttodelete);
 
-            if (department == null)
+            if (depttodelete == null)
             {
                 isvalid = false;
                 MessageBox.Show("Department not found, please verify your entry.");
             }
+
             // if person is assigned to department, cannot delete department
+            // if a department is in the assignments table, then it does have someone assigned to it
+            assignment deptfoundinassignments = _studentprojectEntities.assignments.FirstOrDefault(x => x.deptID == departmentobject.deptID);
+            if (deptfoundinassignments != null)
+            {
+                isvalid = false;
+                MessageBox.Show("Department has one or more employees assigned to it. Please unassign employees before attempting to delete the department.");
+            }
 
 
             if (isvalid)
             {
                 try
                 {
-                    _studentprojectEntities.departments.Remove(department);
+                    _studentprojectEntities.departments.Remove(departmentobject);
 
                     _studentprojectEntities.SaveChangesAsync();
 
